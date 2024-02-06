ea5e8c334bed3f8dcd4c6cbe5b378ebda6fba7c5
master <abdurashitovsh19@gmail.com>
2024-02-06 18:29:40 +0500

    first-commit

---
+++ src/main/java/org/example/springbootjenkinsjobs/SpringBootJenkinsJobsApplication.java
@@ -0,0 +1,21 @@
+package org.example.springbootjenkinsjobs;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Log4j2
+public class SpringBootJenkinsJobsApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        log.info("its a continuous integration job ");
+        SpringApplication.run(SpringBootJenkinsJobsApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("it is second logger");
+    }
+}
---
+++ src/test/java/org/example/springbootjenkinsjobs/SpringBootJenkinsJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootjenkinsjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJenkinsJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
